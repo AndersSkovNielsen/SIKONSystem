abe00b7f0e6fa7443f31b97c6c0ee2a24adb39aa
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2019-11-25 14:07:43 +0100

    login virker?

--- SIKONSystem/Controllers/LoginController.cs
+++ SIKONSystem/Controllers/LoginController.cs
@@ -3,32 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SIKONSystem.Models;
+
 //using System.Web.Security;
 
 namespace SIKONSystem.Controllers
 {
     public class LoginController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-    //    [HttpPost]
-    //    public ActionResult Index(Models.Login request)
-    //    {
-    //        if (!ModelState.IsValid) return View(request);
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            return View();
+        }
 
-    //        if (!string.IsNullOrEmpty(request.Username) && !string.IsNullOrEmpty(request.Password))
-    //        {
-    //           // FormsAuthentication.SetAuthCookie(request.Username, false);
-    //           //& return Redirect(FormsAuthentication.GetRedirectUrl(request.Username, false));
-    //           return
-    //        }
+        //    [HttpPost]
+        //    public ActionResult Index(Models.Login request)
+        //    {
+        //        if (!ModelState.IsValid) return View(request);
 
-    //        ViewBag.Failed = true;
-    //        return View(request);
-    //    }
+        //        if (!string.IsNullOrEmpty(request.Username) && !string.IsNullOrEmpty(request.Password))
+        //        {
+        //           // FormsAuthentication.SetAuthCookie(request.Username, false);
+        //           //& return Redirect(FormsAuthentication.GetRedirectUrl(request.Username, false));
+        //           return
+        //        }
+
+        //        ViewBag.Failed = true;
+        //        return View(request);
+        //    }
     }
     
 }
---
+++ SIKONSystem/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIKONSystem.Models
+{
+    public class User
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Usernames must be valid email address")]
+        [MinLength(6,ErrorMessage="address minimum 6 characters")]
+        public string Username { get; set; }
+        [Required]
+        //[CommonPasswords(ErrorMessage = "This password is too common")]
+        [DataType(DataType.Password)]
+        [ MinLength(8, ErrorMessage = "This password is too short")]
+        public string Password { get; set; }
+    }
+}
