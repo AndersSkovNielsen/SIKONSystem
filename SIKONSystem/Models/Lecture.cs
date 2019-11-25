597b126a9ed93f8c1a489883407990ea7c9d1161
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2019-11-25 15:28:34 +0100

    Lecture Room  og Booking added

---
+++ SIKONSystem/Models/Lecture.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIKONSystem.Models
+{
+    public class Lecture
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _timeFrame;
+
+        public int TimeFrame
+        {
+            get { return _timeFrame; }
+            set { _timeFrame = value; }
+        }
+
+        //enum _categoryColour
+        //{
+           
+        //}
+
+
+        private Room _room;
+
+        public Room Room
+        {
+            get { return _room; }
+            set { _room = value; }
+        }
+
+        private Collection<User> _partakers;
+
+        public Collection<User> Partakers
+        {
+            get { return _partakers; }
+            set { _partakers = value; }
+        }
+
+        private string _speaker;
+
+        public string Speaker
+        {
+            get { return _speaker; }
+            set { _speaker = value; }
+        }
+
+        private Queue<User> _waitingList;
+
+        public Queue<User> WaitingList
+        {
+            get { return _waitingList; }
+            set { _waitingList = value; }
+        }
+
+
+    }
+}
---
+++ SIKONSystem/Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace SIKONSystem.Models
+{
+    public class Room
+    {
+        private int _capacity;
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set { _capacity = value; }
+        }
+
+        public Room(int capacity)
+        {
+            _capacity = capacity;
+        }
+    }
+}
