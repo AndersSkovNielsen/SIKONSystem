22316d66e46ec624dfd76f48494aef097ca2da63
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2019-11-28 10:48:37 +0100

    Booking gjort til Singleton og refactoreret

---
+++ SIKONSystem/Models/BookingSingleton.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace SIKONSystem.Models
+{
+    public class BookingSingleton
+    {
+        private static int _singleton;
+        
+        public int create()
+        {
+            if (_singleton == 0)
+            {
+              BookingSingleton Booker= new BookingSingleton();
+            }
+
+            return _singleton;
+
+        }
+
+        public bool Partake(Lecture L, User U)
+        {
+            if (L.Partakers.Count < L.Room.Capacity)
+            {
+                return Attend(U, L);
+            }
+            else
+            {
+                L.WaitingList.Enqueue(U);
+                return false;
+            }
+
+        }
+
+        public bool Cancel(Lecture L, User U)
+        {
+            bool returnVal;
+            if (L.Partakers.Contains(U))
+            {
+                L.Partakers.Remove(U);
+
+                if (L.WaitingList.Peek() != null)
+                {
+                    returnVal = Attend(L.WaitingList.Dequeue(), L);
+                }
+                else returnVal = false;
+            }
+            else throw  new Exception("Fejl i afmelding: Du var ikke tilmeldt denne begivenhed");
+            return returnVal;
+        }
+
+        private bool Attend(User U, Lecture L)
+        {
+            L.Partakers.Add(U);
+            U.Agenda.Lectures.Add(L);
+            return true;
+        }
+
+        private BookingSingleton()
+        {
+            _singleton++;
+        }
+    }
+}
--- SIKONSystem/Models/User.cs
+++ SIKONSystem/Models/User.cs
@@ -6,18 +6,36 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace SIKONSystem.Models
-{
+{/// <summary>
+/// SikonUser class
+/// </summary>
     public class User
     {
         public string Name { get; set; }
         [Required]
+
         [EmailAddress(ErrorMessage = "Email skal være gyldig.")]
         [MinLength(6, ErrorMessage="Email kan ikke være mindre end 6 karakterer.")]
         public string Email { get; set; }
+
         [Required]
         //[CommonPasswords(ErrorMessage = "This password is too common")]
         [DataType(DataType.Password)]
         [ MinLength(8, ErrorMessage = "Password skal være over 8 karakterer.")]
         public string Password { get; set; }
+
+        private Schedule _agenda;
+
+        public Schedule Agenda
+        {
+            get { return _agenda; }
+            set { _agenda = value; }
+        }
+
+
+        public User()
+        {
+            _agenda = new Schedule();
+        }
     }
 }
