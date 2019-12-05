b3282d356ebc53016a95a7ac6bc5ef38d99cf23d
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2019-12-05 12:35:12 +0100

    Mere Displaymodel

--- SIKONSystem/DisplayModel/LectureDisplayModel.cs
+++ SIKONSystem/DisplayModel/LectureDisplayModel.cs
@@ -22,26 +22,28 @@ namespace SIKONSystem.DisplayModel
         public SelectList Rooms { get /*{ return  ;}*/; set; }
         public string RoomName { get; set; }
 
-        public string GatherSchedule(int noOfRooms, int noOfTimeBlocks, List<Lecture> lectures)
-        {
+        public async Task<string> GatherSchedule(int noOfRooms, int noOfTimeBlocks, List<Lecture> lectures)
+        {// Hvordan kan vi tilføje asyncronitet til denne metode?? -Frederik
             RoomDisplayList=new List<Room>();
             Room r1 = new Room(20, "A4 Køge");
             Room r2 = new Room(20, "B5 KBH");
+            Room r3 = new Room(20, "C5 LGH");
 
             RoomDisplayList.Add(r1);
             RoomDisplayList.Add(r2);
+            RoomDisplayList.Add(r3);
 
-            RoomDisplayList.Sort((x, y) => string.Compare(x.Name, y.Name));
+             RoomDisplayList.Sort((x, y) => string.Compare(x.Name, y.Name));
 
             string headerstring ="";
-            for (int i = 0; i < noOfRooms; i++)
+           for (int i = 0; i < noOfRooms; i++)
             {
                 headerstring = $"{headerstring}" + $"<th>| {RoomDisplayList[i].Name} |</th>";
             }
             
             List<string> retlist= new List<string>();
             string retValue = "";
-            retValue=retValue+$"<thead><tr>{headerstring}</tr></thead>";
+            retValue=retValue+$"<thead><tr/><th/>{headerstring}</tr></thead>";
 
            
 
@@ -54,9 +56,9 @@ namespace SIKONSystem.DisplayModel
             return retValue;
         }
 
-        public string PrintSchedule(string task)
+        public string PrintSchedule(Task<string> task)
         {
-            return task;
+            return task.Result;
         }
 
 
